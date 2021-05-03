f4d7b2fff392c93b6bb2b22f46aad7267ad10bc6
DarianStuit <55897445+DarianStuit@users.noreply.github.com>
2021-05-02 18:06:10 -0700

    Added a van firebase nuke

---
+++ ClassDefinitions/Service.cs
@@ -0,0 +1,79 @@
+﻿using Firebase.Database;
+
+namespace MobileVaccination.ClassDefinitions
+{
+    using Newtonsoft.Json;
+    public class Appointment
+    {
+
+
+        public Patient prospect { get; set; }
+        public Destination destination { get; set; }
+        public Point[] pointList { get; set; }
+        public string InitialTime { get; set; }
+        public bool acepted { get; set; }
+        public string vaccinated { get; set; }
+        public string active { get; set; }
+
+        //public int key { get; set; }
+        public string key { get; set; }
+
+    }
+
+    public class Patient
+    {
+        public string userName;
+        public string FirstName;
+        public string LastName;
+        public string SS;
+        public string Age;
+        public int uid;
+        public string image;
+        public string userCellphone;
+    }
+    public class Driver
+    {
+        public int companyColor;
+
+    }
+
+    public class Point
+    {
+        public double lat;
+        public double lng;
+        public double time;
+    }
+    public class Destination
+    {
+        public double lat;
+        public double lng;
+        public string destinationName;
+    }
+    public class Company
+    {
+        public string companyName { get; set; }
+        public string status { get; set; }
+    }
+    public class Status
+    {
+        public int code { get; set; }
+        public ServerTimeStamp time { get; } = new ServerTimeStamp();
+    }
+
+    public class VanInfo//prototype to add van info
+    {
+        public int plate { get; set; }
+        public string id { get; set; }
+        public ServerTimeStamp time { get; } = new ServerTimeStamp();
+    }
+    public class Response
+    {
+        public bool success;
+        public int index;
+        public string message;
+        public string companyId;
+        public string color;
+    }
+
+}
+
---
+++ ClassDefinitions/Van.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//may not need all of these GMap.NETs but added them just in case
+using GMap.NET;
+using GMap.NET.MapProviders;
+using GMap.NET.WindowsForms;
+using GMap.NET.WindowsForms.Markers;
+using GMap.NET.WindowsForms.ToolTips;
+
+namespace MobileVaccination.ClassDefinitions
+{
+    public class Van
+    {
+        public PointLatLng Position;
+        public GMapMarker PositionMarker;
+        public string Make;
+        public string Model;
+        public int Vials;
+        public double TimeSinceRefill;
+        public bool HasAppointment;
+        public bool HasRoute;
+        public Appointment appointment;
+        public string CarPlate;
+        public string Vid;
+        public System.Drawing.Color routeColor;
+        public GMap.NET.MapRoute route;
+        public string key;
+        public Van()
+        {
+            Position.Lat = 0;
+            Position.Lng = 0;
+            PositionMarker = new GMarkerGoogle(Position, GMarkerGoogleType.green_dot);
+            PositionMarker.ToolTipText = Vid;
+            PositionMarker.ToolTipMode = MarkerTooltipMode.Always;
+            Vials = 0;
+            TimeSinceRefill = 0.0;
+            HasAppointment = false;
+            HasRoute = false;
+            appointment = null;
+            //default routeColor (give each van a unique color after creating them)
+            routeColor = System.Drawing.Color.Blue;
+        }
+    }
+}
